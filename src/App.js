c618dd4dc2ca0ba27200e73e5e2c5bd73df2638a
satywan <satywankumar646@gmail.com>
2023-03-03 21:44:16 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import AddCategory from "./Component/AddCategory";
+import Categorylist from "./Component/Categorylist";
+import Detail from "./Component/Detail";
+import RootLayout from "./Component/RootLayout";
+import Update from "./Component/Update";
+
+const router = createBrowserRouter([
+ {
+  path:'',
+  element:<RootLayout/>,
+  children:[
+    {index:true,element:<Categorylist/>},
+    {path:'add-category',element:<AddCategory/>},
+    {path:'category-list',element:<Categorylist/>},
+    {path:'detail/:id',element:<Detail/>},
+    {path:'edit/:id',element:<Update/>}
+  ]
+ }
+])
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <RouterProvider router={router}></RouterProvider> 
+    </>
   );
 }
 
